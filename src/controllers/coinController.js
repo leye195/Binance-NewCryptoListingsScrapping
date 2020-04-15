6b0c40afab679c0f86798ee510f3a854d47015b1
leye195 <leye195@naver.com>
2020-04-16 03:24:31 +0900

    test

--- src/controllers/coinController.js
+++ src/controllers/coinController.js
@@ -3,25 +3,25 @@ import { extractNewListing } from "../binance";
 import binanceNoticeModel from "../model/binanceNoticeModel";
 let job = null;
 export const startSchedule = async (req, res, next) => {
-  //let rule = new schedule.RecurrenceRule();
+  let rule = new schedule.RecurrenceRule();
   try {
-    //job = schedule.scheduleJob("*/5 * * * * *", async () => {
-    //스케쥴링 활용 실행 반복 사용자 임의 수정 가능
-    const coinList = await extractNewListing("will list"); //원하는 키워드 입력
-    [].forEach.call(coinList, async (item) => {
-      const notice = await binanceNoticeModel.findOne({ title: item.title });
-      if (!notice) {
-        await binanceNoticeModel.create({
-          title: item.title,
-          link: item.link,
-          coin: item.coin,
-          updatedAt: item.updatedAt,
-        });
-      }
+    job = schedule.scheduleJob("*/5 * * * * *", async () => {
+      //스케쥴링 활용 실행 반복 사용자 임의 수정 가능
+      const coinList = await extractNewListing("will list"); //원하는 키워드 입력
+      [].forEach.call(coinList, async (item) => {
+        const notice = await binanceNoticeModel.findOne({ title: item.title });
+        if (!notice) {
+          await binanceNoticeModel.create({
+            title: item.title,
+            link: item.link,
+            coin: item.coin,
+            updatedAt: item.updatedAt,
+          });
+        }
+      });
+      console.log("Scraping 진행");
     });
-    console.log("Scraping 진행");
     res.send("Scraping 시작");
-    //});
   } catch (e) {
     console.error(e);
     //job.cancel();
